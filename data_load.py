0ad5b01822cbd88da82258cd1930d024c04109f6
Zixiong SU <soshiyuu@gmail.com>
2020-06-29 18:26:45 +0900

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -35,9 +35,9 @@ class TrainDataset(Dataset):
     def __getitem__(self, idx):
 
 
-        np_file_dir = [b for b in os.listdir(self.path) if b[0] != "."]
+        np_file_dir = os.listdir(self.path)
         selected_file = random.sample(np_file_dir, 1)[0]  # select random speaker          
-        utterances = np.load(selected_file)
+        utterances = np.load(os.path.join(self.path,selected_file))
 
         index = np.random.randint(0, utterances.shape[0], self.utter_num)   # select M utterances per speaker
         selected_utters = torch.tensor(utterances[index])
@@ -68,7 +68,7 @@ class TestDataset(Dataset):
 
         np_file_dir = os.listdir(self.path)
         selected_file = random.sample(np_file_dir, 1)[0]  # select random speaker          
-        embs = np.load(selected_file)
+        embs = np.load(os.path.join(self.path,selected_file))
         
         index = np.random.randint(0, embs.shape[0], self.utter_num)   # select M utterances per speaker
         selected_embs = torch.tensor(embs[index])
